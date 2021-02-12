12e54fe25d30fcaeea711036c014aed88488c9ff
siloob <antonin.garanto@ecole.ensicaen.fr>
2021-02-12 10:45:55 +0100

    add swagger ui doc

--- rapdata/rapapi/urls.py
+++ rapdata/rapapi/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
+
+from rest_framework_swagger.views import get_swagger_view
 from rapapi import views
 
 router = routers.DefaultRouter()
+schema_view = get_swagger_view(title='RAPDATA API')
+
 router.register(r'artists', views.ArtistViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('docs', schema_view)
 ]
--- rapdata/rapdata/settings.py
+++ rapdata/rapdata/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'data.apps.DataConfig',
     'rapapi.apps.RapapiConfig',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -180,5 +181,6 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
