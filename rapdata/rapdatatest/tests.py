bb1edd54355f3add04aef38ebcaa25103c6f5104
siloob <antonin.garanto@ecole.ensicaen.fr>
2021-03-01 21:15:33 +0100

    end test for API

--- rapdata/rapdatatest/tests.py
+++ rapdata/rapdatatest/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, force_authenticate
 
-from rapapi.views import ArtistViewSet
+from rapapi.views import ArtistViewSet, UserCreateView, GetRequestLimit
 from rapapi.models import Artist
 
-from rapdatatest.testutils.utils import create_user, get_artist
+from rapdatatest.testutils.utils import create_user, get_artist, get_user_from_username
 
 # Create your tests here.
 
@@ -14,30 +14,88 @@ class ArtistViewSetTest(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.user = create_user()
+        self.artist = get_artist()
+        self.view = ArtistViewSet.as_view({'get':'list'}) 
         
     def test_get_all_unauth(self):
         request = self.factory.get('/api/artists')
-        response = ArtistViewSet.as_view({'get': 'list'})(request)
+        response = self.view(request)
         
         self.assertEqual(response.status_code, 401)
 
     def test_get_all_auth_as_user(self):
         request = self.factory.get('/api/artists')
         force_authenticate(request, user=self.user)
-        response = ArtistViewSet.as_view({'get': 'list'})(request)
+        response = self.view(request)
 
         self.assertEqual(response.status_code, 200)
 
     def test_get_artist_by_id(self):
-        artist = get_artist()
-        request = self.factory.get('/api/artists/%i' % artist.pk)
+        request = self.factory.get('/api/artists/%i' % self.artist.pk)
         force_authenticate(request, user=self.user)
-        response = ArtistViewSet.as_view({'get': 'list'})(request)
+        response = self.view(request)
 
         self.assertEqual(response.status_code, 200)
         
-        self.assertEqual(response.data['results'][0]['name'], 'Kool Shen')
-        self.assertEqual(response.data['results'][0]['id_music_story'], 5094)
-        self.assertEqual(response.data['results'][0]['id_rap_genius'], 12547)
-        self.assertEqual(response.data['results'][0]['genius_followers'], 28)
+        self.assertEqual(response.data['results'][0]['name'], self.artist.name)
+        self.assertEqual(response.data['results'][0]['id_music_story'], self.artist.id_music_story)
+        self.assertEqual(response.data['results'][0]['id_rap_genius'], self.artist.id_rap_genius)
+        self.assertEqual(response.data['results'][0]['genius_followers'], self.artist.genius_followers)
 
+class UserCreateTest(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = create_user()
+        self.view = UserCreateView.as_view()
+        self.data = {
+            'username': 'anka',
+            'password': 'bahtiens'
+        }
+
+    def test_create_user_from_user(self):
+        request = self.factory.post('/api/user/', self.data)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        response.render()
+        
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_create_user_from_admin(self):
+        self.user.is_staff = True
+        self.user.save()
+
+        request = self.factory.post('/api/user/', self.data)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        response.render()
+
+        user_created = get_user_from_username(self.data['username'])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data['token'])
+        self.assertEqual(user_created.username, self.data['username'])
+        self.assertEqual(user_created.password, self.data['password'])
+
+class GetLimitsRequestsTest(TestCase):
+    
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = create_user()
+        self.view = GetRequestLimit.as_view()
+
+    def test_get_limits_unauth(self):
+        request = self.factory.get('/api/requests')
+        response = self.view(request)
+        response.render()
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_limits_auth(self):
+        request = self.factory.get('/api/requests')
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        response.render()
+
+        self.assertEqual(response.status_code, 200)
--- rapdata/rapdatatest/testutils/utils.py
+++ rapdata/rapdatatest/testutils/utils.py
@@ -4,12 +4,16 @@ from data.models import Artist
 
 
 def create_user():
-    user = User.objects.create(username='petrou', password="bahtiens")
-    user_group = Group.objects.create(name='user')
-    user_group.user_set.add(user)
-    user_group.save()
+    user, created = User.objects.get_or_create(username='petrou', password="bahtiens")
+    if created:
+        user_group = Group.objects.create(name='user')
+        user_group.user_set.add(user)
+        user_group.save()
 
     return user
 
 def get_artist():
     return Artist.objects.all().first()
+
+def get_user_from_username(username):
+    return User.objects.get(username=username)
